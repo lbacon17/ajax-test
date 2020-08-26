00c2b9bd409ae9342d5fe4e9420ddfe30ec1f66a
lbacon17 <louis.bacon17@icloud.com>
2020-08-26 16:40:21 +0000

    implement pagination

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const baseURL = "https://ci-swapi.herokuapp.com/api/";
-
-function getData(type, cb) {
+function getData(url, cb) {
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", baseURL + type + "/");
+    xhr.open("GET", url);
     xhr.send();
 
     xhr.onreadystatechange = function () {
@@ -24,11 +22,27 @@ function getTableHeaders(obj) {
     return `<tr>${tableHeaders}</tr>`
 }
 
-function writeToDocument(type) {
+function generatePaginationButtons(next, prev) {
+    if (next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`
+    } else if (next && !prev) {
+        return `<button onclick="writeToDocument('${next}')">Next</button>`
+    } else if (!next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`
+    }
+}
+
+function writeToDocument(url) {
     var tableRows = [];
     var el = document.getElementById("data");
     el.innerHTML = "";
-    getData(type, function(data) {
+    getData(url, function(data) {
+        var pagination;
+        if (data.next || data.previous) {
+            pagination = generatePaginationButtons(data.next, data.previous)
+        }
+
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
@@ -45,6 +59,6 @@ function writeToDocument(type) {
             //el.innerHTML += "<p>" + item.name + "</p>";
         })
 
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`
     });
 }
